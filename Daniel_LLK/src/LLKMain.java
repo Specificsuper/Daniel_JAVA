ddbc688f6bb32b391620fb6792e14ae4a7b268d1
Specificsuper <Daniel_JAVA>
2017-12-23 17:43:12 +0800

    连连看DEMO

---
+++ Daniel_LLK/src/LLKMain.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LLKMain extends Application{
+	private static TextField tf=new TextField();
+	public void start(Stage stage) throws Exception {
+
+	}
+		public static void main(String[] args) {
+			Application.launch(args);
+		}
+
+}
